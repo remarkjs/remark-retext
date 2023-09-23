d98329e2ae8b7301736389c927ad39df2d782f1d
Titus Wormer <tituswormer@gmail.com>
2023-09-23 15:42:39 +0200

    Update `@types/mdast`, utilities, etc

--- test.js
+++ test.js
@@ -1,34 +1,39 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {unified} from 'unified'
+import {ParseEnglish} from 'parse-english'
 import remarkParse from 'remark-parse'
-import retextEnglish, {Parser as RetextEnglish} from 'retext-english'
+import retextEnglish from 'retext-english'
 import remarkStringify from 'remark-stringify'
 import retextStringify from 'retext-stringify'
 import remarkRetext from './index.js'
 
 test('remarkRetext', async function (t) {
+  await t.test('should throw when w/o parser or processor', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how missing options is handled.
+      unified().use(remarkRetext).freeze()
+    }, /Expected `parser` \(such as from `parse-english`\) or `processor` \(a unified pipeline\) as `parserOrProcessor`/)
+  })
+
   await t.test('should mutate', async function () {
-    assert.equal(
-      unified()
-        .use(remarkParse)
-        .use(remarkRetext, RetextEnglish)
-        .use(retextStringify)
-        .processSync('## Hello, world! ##')
-        .toString(),
-      'Hello, world!'
-    )
+    const file = await unified()
+      .use(remarkParse)
+      .use(remarkRetext, ParseEnglish)
+      .use(retextStringify)
+      .process('## Hello, world! ##')
+
+    assert.equal(String(file), 'Hello, world!')
   })
 
   await t.test('should bridge', async function () {
-    assert.equal(
-      unified()
-        .use(remarkParse)
-        .use(remarkRetext, unified().use(retextEnglish))
-        .use(remarkStringify)
-        .processSync('## Hello, world! ##')
-        .toString(),
-      '## Hello, world!\n'
-    )
+    const file = await unified()
+      .use(remarkParse)
+      // @ts-expect-error: to do.
+      .use(remarkRetext, unified().use(retextEnglish))
+      .use(remarkStringify)
+      .process('## Hello, world! ##')
+
+    assert.equal(String(file), '## Hello, world!\n')
   })
 })
